2634eb40a85147ea6e77aaccd59b4a51275a1db9
haroonali444 <haroonali8838@gmail.com>
2025-09-14 02:43:05 -0400

    Add sample app + Snyk workflow setup

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# security-test-app
+# Security Test App
+This is a sample Node.js app used to test Snyk integration.
---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const _ = require("lodash");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello, Snyk Test App!");
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log("App listening on port " + PORT);
+});
